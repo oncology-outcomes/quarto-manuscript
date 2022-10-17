1213ec249a8f7e6cbde7ee492edd858ae91d4842
Matthew T. Warkentin <warkentin@lunenfeld.ca>
2022-10-17 14:36:24 -0600

    Separate quarto extensions

--- _extensions/hide-header/hide-header.lua
+++
@@ -1,8 +0,0 @@
--- Author: Matt Warkentin <warkentin@lunenfeld.ca>
--- Remove headers tagged with the `.hide` class
-function Header(elem)
-  if elem.classes[1] == 'hide' then
-    return pandoc.Null()
-  end
-  return elem
-end
